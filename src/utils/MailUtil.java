653147f5a437d29a81c30de49ae7eb3cb46cf4fb
congnhat234 <31690712+congnhat234@users.noreply.github.com>
2018-12-14 23:22:29 +0700

    # Mail Util

---
+++ src/utils/MailUtil.java
@@ -0,0 +1,47 @@
+package utils;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
+public class MailUtil {
+
+	/**
+	   Outgoing Mail (SMTP) Server
+	   requires TLS or SSL: smtp.gmail.com (use authentication)
+	   Use Authentication: Yes
+	   Port for SSL: 465
+	 */
+	public static void sendMail(String email) {
+		final String fromEmail = "nhattvcn@gmail.com"; //requires valid gmail id
+		final String password = "Congnhat234"; // correct password for gmail id
+		final String toEmail = email; // can be any email id 
+		String subject = "Thay đổi mật khẩu cho tài khoản BachKhoa Forums của bạn";
+		String content = "Mật khẩu mới của bạn là 123456";
+		
+		System.out.println("SSLEmail Start");
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com"); //SMTP Host
+		props.put("mail.smtp.socketFactory.port", "465"); //SSL Port
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory"); //SSL Factory Class
+		props.put("mail.smtp.auth", "true"); //Enabling SMTP Authentication
+		props.put("mail.smtp.port", "465"); //SMTP Port
+		
+		Authenticator auth = new Authenticator() {
+			//override the getPasswordAuthentication method
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(fromEmail, password);
+			}
+		};
+		
+		Session session = Session.getDefaultInstance(props, auth);
+		System.out.println("Session created");
+	        EmailUtil.sendEmail(session, toEmail,subject, content);
+
+	}
+
+}
+
